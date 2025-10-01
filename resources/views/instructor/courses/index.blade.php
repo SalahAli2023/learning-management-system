07c51102ec65fb775c17624096985eb8d080541e
salah <salahalhimyari2023@gmail.com>
2025-10-02 01:36:42 +0300

    feat(ui): update courses/index.blade.php lessons/index.blade.php lessons/show.blade.php view

--- resources/views/instructor/courses/index.blade.php
+++ resources/views/instructor/courses/index.blade.php
@@ -1,7 +1,18 @@
-<x-app-layout>
+{{-- <x-app-layout>
     <courses-index
         :courses='@json($courses)'
         :user='@json(auth()->user())'
         create-route="{{ route('instructor.courses.create') }}"
+    ></courses-index>
+</x-app-layout> --}}
+
+<x-app-layout>
+    <courses-index
+        :courses='@json($courses)'
+        :user-role='@json(auth()->user()->role)'
+        :user='@json(auth()->user())'
+        :enrollments='@json($enrollments ?? [])'
+        create-route="{{ route('instructor.courses.create') }}"
+        csrf-token="{{ csrf_token() }}"
     ></courses-index>
 </x-app-layout>
