70629257bbac4da38d7c6e0f8c4241b139d81a93
salah <salahalhimyari2023@gmail.com>
2025-10-02 01:58:12 +0300

    feat(routes): add routes of courses & lessons & enrollment & student pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Instructor\CourseController;
+use App\Http\Controllers\Instructor\LessonController;
+use App\Http\Controllers\EnrollmentController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -28,15 +31,32 @@ Route::middleware('auth')->group(function () {
     })->middleware('role:admin')->name('dashboard');
 });
 
-Route::middleware(['auth','role:instructor,admin'])->prefix('instructor')
-    ->name('instructor.')->group(function () {
+Route::middleware(['auth','verified'])->prefix('instructor')
+    ->name('instructor.')->group(function () {  
+        
         Route::resource('courses', CourseController::class);
-         //  routes for restore 
+        
+        //  routes for restore 
         Route::post('courses/{course}/restore', [CourseController::class, 'restore'])
             ->name('courses.restore');
             
         Route::delete('courses/{course}/force', [CourseController::class, 'forceDelete'])
             ->name('courses.forceDelete');
+
+        
+        Route::resource('courses.lessons', LessonController::class);
+        Route::post('/lessons/{lesson}/restore', [LessonController::class, 'restore'])
+            ->name('lessons.restore');
+
+        Route::post('/lessons/{lesson}/force', [LessonController::class, 'forceDelete'])
+            ->name('lessons.forceDelete');
+
+        Route::resource('courses.enrollments', EnrollmentController::class);
+        Route::get('/courses/{course}/enrollments', [EnrollmentController::class, 'index'])
+            ->name('enrollments.index');
+            
+        Route::put('/courses/{course}/enrollments/{enrollment}', [EnrollmentController::class, 'update'])
+            ->name('enrollments.update');
     });
 
 Route::get('/test-auth', function () {
@@ -61,4 +81,21 @@ Route::get('/test-policy', function () {
     ]);
 });
 
+
+// routes/web.php
+
+
+
+Route::middleware(['auth', 'role:student'])->prefix('student')->group(function () {
+    Route::get('/enrollments', [EnrollmentController::class, 'myEnrollments'])
+            ->name('student.enrollments.index');
+    // Courses
+    Route::get('/courses', [CourseController::class, 'index'])->name('student.courses.index');
+    Route::get('/courses/{course}', [StudentController::class, 'show'])->name('student.courses.show');
+    Route::post('/courses/{course}/enroll', [StudentController::class, 'enroll'])->name('student.courses.enroll');
+    Route::get('/courses/{course}/progress', [StudentController::class, 'progress'])->name('student.courses.progress');
+    
+    // Lessons
+    Route::get('/courses/{course}/lessons/{lesson}', [StudentController::class, 'play'])->name('student.lessons.show');
+});
 require __DIR__.'/auth.php';
